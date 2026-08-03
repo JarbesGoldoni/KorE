4cf0d878a037824d0ec077f897ed69981501574c
Jarbes Goldoni <jarbes.d@gmail.com>
2026-08-03 19:31:25 -0300

    feat(project): add Mix project configuration and CLI project templates

---
+++ mix.exs
@@ -0,0 +1,29 @@
+defmodule Kore.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :kore,
+      version: "0.1.0",
+      elixir: "~> 1.15",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      escript: escript(),
+      test_pattern: "*_test.exs"
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    []
+  end
+
+  defp escript do
+    [main_module: Kore.CLI]
+  end
+end
